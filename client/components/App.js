211fb1b56c2e7325eca0a93871d95057344ff76b
Kin Chan <xkinchanx@gmail.com>
2018-10-12 17:21:33 -0700

    [prog] on mount, app will grab any saved data

--- client/components/App.js
+++ client/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router';
 import { BrowserRouter } from 'react-router-dom';
+import axios from 'axios';
 import './App.css';
 
 import Draft from './Draft';
@@ -9,18 +10,47 @@ import View from './View';
 import Nav from './Nav';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      scripts: null
+    }
+  }
+
+  componentDidMount() {
+    axios.get('/api')
+      .then(({ data: scripts }) => {
+        this.setState({ scripts });
+      })
+      .catch((err) => console.error(err));
+  }
+
   render() {
+    let { scripts } = this.state;
     return (
-      <BrowserRouter>
-        <div className="App">
-          <Nav />
-          <Switch>
-            <Route exact path="/" component={View}/>
-            <Route path="/edit" component={Edit}/>
-            <Route path="/draft" component={Draft}/>
-          </Switch>
-        </div>
-      </BrowserRouter>
+      <div>
+        {scripts &&
+          <BrowserRouter>
+            <div className="App">
+              <Nav />
+              <Switch>
+                <Route exact path="/"
+                  render={() => <View />}
+                  // component={View}
+                  />
+                <Route path="/edit" 
+                  render={() => <Edit scripts={this.state.scripts}/>}
+                  // component={Edit}
+                  />
+                <Route path="/draft" 
+                  render={() => <Draft />}
+                  // component={Draft}
+                  />
+              </Switch>
+            </div>
+          </BrowserRouter>
+        }
+      </div>
     );
   }
 }
