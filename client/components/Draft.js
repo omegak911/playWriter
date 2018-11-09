8a731d55624c0e82e9d0d0842848451e487d72ed
Kin Chan <xkinchanx@gmail.com>
2018-11-09 11:54:08 -0800

    [prog] draft component displaying with ability to render + update

--- client/components/Draft.js
+++ client/components/Draft.js
@@ -1,26 +1,55 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import styled from 'styled-components';
 
 class Draft extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      play: [1,2],
+      draft: 'one',
     }
   }
 
+  componentDidMount() {
+    axios.get('api/draft')
+      .then(({ data }) => this.setState({ draft: data }))
+      .catch(err => console.error(err))
+  }
+
+  saveDraft = () => {
+    let { draft } = this.state;
+    axios.post('api/draft', { draft })
+    .then(({ data }) => console.log(data))
+    .catch(err => console.error(err))
+  }
+
+  updateDraft = (e) => {
+    this.setState({ draft: e.target.value })
+  }
+
   render() {
-    let { play } = this.state;
+    let { draft } = this.state;
     return (
-      <div>
-        Welcome to Draft
-        {play.map((part, index) => 
-          <div key={index}>
-            { part }
-          </div>
-        )}
-      </div>
+      <StyledCenter>
+        <button type="button" onClick={this.saveDraft}>Save Draft</button>
+        <p>Here we will figure out the storyline</p>
+        <br/>
+        <StyledTextarea name="" id="" cols="30" rows="10" value={draft} onChange={this.updateDraft}></StyledTextarea>
+      </StyledCenter>
     )
   }
 }
+
+const StyledCenter = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const StyledTextarea = styled.textarea`
+  height: 80vh;
+  width: 90%;
+`;
 
 export default Draft;
